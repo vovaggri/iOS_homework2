ce93c2cbae978ebe764a079f11e4a16e91d417eb
Vladimir Grigoryev <79272578+vovaggri@users.noreply.github.com>
2024-12-07 15:18:11 +0300

    Changes for HW4

--- vlgrigorevPW2/vlgrigorevPW2/SceneDelegate.swift
+++ vlgrigorevPW2/vlgrigorevPW2/SceneDelegate.swift
@@ -19,7 +19,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
         let window = UIWindow(windowScene: windowScene)
-        window.rootViewController = UINavigationController(rootViewController: Assembler.build())
+        window.rootViewController = UINavigationController(rootViewController: WishMakerViewController())
         self.window = window
         window.makeKeyAndVisible() 
     }
